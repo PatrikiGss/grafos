e3f82a5e5299d2713e5e0f8dcd05f5a156a16756
PatrikiGss <patriki7771@gmail.com>
2025-02-11 17:08:44 -0300

    first commit

---
+++ src/grafos/Algoritmos.java
@@ -0,0 +1,19 @@
+package grafos;
+
+import java.util.ArrayList;
+
+/**
+ * Nesta classe devem ser implementados todos os métodos de grafos de forma estática
+ * @author vilson.junior
+ */
+public class Algoritmos {
+    
+    public static ArrayList<Vertice> percorreLargura(Grafo g, Vertice inicio) {
+        ArrayList<Vertice> resultado = new ArrayList();
+
+        return resultado;
+    }
+    
+    //
+    //
+}
